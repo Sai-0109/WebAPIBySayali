7198b9ff4ef3b9a7234775fde6c0ffdd77dbbec1
sayal <sayal@SAYALI-PC>
2024-01-17 23:09:59 +0530

    added b controller

---
+++ WebApiFirst/Controllers/BController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiFirst.Controllers
+{
+    public class BController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
